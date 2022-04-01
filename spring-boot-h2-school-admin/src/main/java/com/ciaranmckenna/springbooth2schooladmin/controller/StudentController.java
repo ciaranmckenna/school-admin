00f1a97a0fb108e8e530d10df77063883deb2405
Ciaran McKenna <hello@ciaranmckenna.com>
2022-04-01 18:18:06 +0100

    Adding delete and post functionality

--- spring-boot-h2-school-admin/src/main/java/com/ciaranmckenna/springbooth2schooladmin/controller/StudentController.java
+++ spring-boot-h2-school-admin/src/main/java/com/ciaranmckenna/springbooth2schooladmin/controller/StudentController.java
@@ -3,8 +3,11 @@ package com.ciaranmckenna.springbooth2schooladmin.controller;
 import com.ciaranmckenna.springbooth2schooladmin.model.Student;
 import com.ciaranmckenna.springbooth2schooladmin.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,5 +28,17 @@ public class StudentController {
     private Student getStudent(@PathVariable("id") int id)
     {
         return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("/student/{id}")
+    private void deleteStudent(@PathVariable("id") int id)
+    {
+        studentService.delete(id);
+    }
+    @PostMapping("/student")
+    private int saveStudent(@RequestBody Student student)
+    {
+        studentService.saveOrUpdate(student);
+        return student.getId();
     }
 }
--- spring-boot-h2-school-admin/src/main/java/com/ciaranmckenna/springbooth2schooladmin/service/StudentService.java
+++ spring-boot-h2-school-admin/src/main/java/com/ciaranmckenna/springbooth2schooladmin/service/StudentService.java
@@ -23,4 +23,14 @@ public class StudentService {
     public Student getStudentById(int id) {
         return studentRepository.findById(id).get();
     }
+
+    public void saveOrUpdate(Student student)
+    {
+        studentRepository.save(student);
+    }
+    //deleting a specific record
+    public void delete(int id)
+    {
+        studentRepository.deleteById(id);
+    }
 }
