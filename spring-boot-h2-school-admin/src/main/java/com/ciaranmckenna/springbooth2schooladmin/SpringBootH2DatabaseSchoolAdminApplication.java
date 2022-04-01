0318a2e0ca36c9b70bbd64cd2f807f987a07e3fd
Ciaran McKenna <hello@ciaranmckenna.com>
2022-04-01 17:11:29 +0100

    Adding maven project

---
+++ spring-boot-h2-school-admin/src/main/java/com/ciaranmckenna/springbooth2schooladmin/SpringBootH2DatabaseSchoolAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ciaranmckenna.springbooth2schooladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2DatabaseSchoolAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2DatabaseSchoolAdminApplication.class, args);
+	}
+
+}
